b65cfb6e54ce2f2b5b85c552026ec91491735487
HyunseokLEE <unducklee@gmail.com>
2023-02-09 00:09:06 +0900

    3강 & 4강

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  let [cloth, setCloth] = useState("남자 코트 추천");
+  let [list, setList] = useState("강남 우동 맛집");
+  let [python, setPython] = useState("파이썬독학");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>React blog</div>
+      </div>
+      <div className="list">
+        <h4>{cloth}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{list}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{python}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
