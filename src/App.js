4b5d430e64fb85e185df8b139427f57db2e82a9d
HyunseokLEE <unducklee@gmail.com>
2023-02-10 02:09:37 +0900

    9강

--- src/App.js
+++ src/App.js
@@ -7,75 +7,50 @@ function App() {
     "강남 우동 맛집",
     "파이썬독학",
   ]);
-  let [count, setCount] = useState(0);
+  let [count, setCount] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
   return (
     <div className="App">
-      {modal === true ? <Modal /> : null}
       <div className="black-nav">
         <div>React blog</div>
-        <button
-          onClick={() => {
-            let copy = [...list]; //["남자 코트 추천","강남 우동 맛집","파이썬독학",]
-            copy.sort(); //["강남 우동 맛집","남자 코트 추천","파이썬독학",]
-            setList(copy); //["강남 우동 맛집","남자 코트 추천","파이썬독학",]
-          }}>
-          sort
-        </button>
       </div>
+
+      {list.map(function (title, i) {
+        return (
+          <div className="list" key={i}>
+            <button
+              onClick={() => {
+                let copy = [...count];
+                copy[i] = copy[i] + 1;
+                setCount(copy);
+              }}>
+              count
+            </button>
+            <div>
+              <span>👍</span>
+              {count[i]}
+            </div>
+            <h4>{title}</h4>
+            <p>2월 17일 발행</p>
+          </div>
+        );
+      })}
+
       <button
         onClick={() => {
           setModal(!modal);
         }}>
         모달
       </button>
-      <div className="list">
-        <h4>
-          {list[0]}
-          <button
-            onClick={() => {
-              setCount(count + 1);
-            }}>
-            👍
-          </button>
-          {count}
-        </h4>
-        <button
-          onClick={() => {
-            let copy = [...list]; //["남자 코트 추천","강남 우동 맛집","파이썬독학",]
-            copy[0] = "여자 코트 추천"; //["여자 코트 추천","강남 우동 맛집","파이썬독학",]
-            setList(copy); //["여자 코트 추천","강남 우동 맛집","파이썬독학",]
-          }}>
-          change
-        </button>
-        <p>2월 17일 발행</p>
-      </div>
-
-      <div className="list">
-        <h4>{list[1]}</h4>
-        <button
-          onClick={() => {
-            let copy = [...list]; //["남자 코트 추천","강남 우동 맛집","파이썬독학",]
-            copy[1] = "신촌 우동 맛집"; //["남자 코트 추천","신촌 우동 맛집","파이썬독학",]
-            setList(copy); //["남자 코트 추천","신촌 우동 맛집","파이썬독학",]
-          }}>
-          change
-        </button>
-        <p>2월 17일 발행</p>
-      </div>
-
-      <div className="list">
-        <h4>{list[2]}</h4>
-        <button
-          onClick={() => {
-            let copy = [...list]; //["남자 코트 추천","강남 우동 맛집","파이썬독학",]
-            copy[2] = "js"; //["남자 코트 추천","강남 우동 맛집","js",]
-            setList(copy); //["남자 코트 추천","강남 우동 맛집","js",]
-          }}>
-          change
-        </button>
-        <p>2월 17일 발행</p>
-      </div>
+      <button
+        onClick={() => {
+          let copy = [...list]; //["남자 코트 추천","강남 우동 맛집","파이썬독학",]
+          copy.sort(); //["강남 우동 맛집","남자 코트 추천","파이썬독학",]
+          setList(copy); //["강남 우동 맛집","남자 코트 추천","파이썬독학",]
+        }}>
+        sort
+      </button>
+      {modal === true ? <Modal /> : null}
     </div>
   );
 }
