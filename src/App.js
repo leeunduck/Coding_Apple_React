86270a6f36dfc2d775f4cb51fdf414d217e99afc
HyunseokLEE <unducklee@gmail.com>
2024-10-14 22:02:34 +0900

    feat: blog_nav

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <h4>블로그</h4>
+      </div>
+    </div>
+  );
 }
 
 export default App;
