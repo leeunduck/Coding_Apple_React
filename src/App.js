31787392b47dbb55c65775742bacb5b07ec76137
HyunseokLEE <unducklee@gmail.com>
2024-10-14 22:26:51 +0900

    feat: learning useState

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  let [writeHeader, b] = useState("남자 코트 추천");
   let posts = "강남 우동 맛집";
   return (
     <div className="App">
@@ -8,8 +10,9 @@ function App() {
         <div>개발 blog</div>
       </div>
       <div className="list">
-        <h4>글제목</h4>
+        <h4>{writeHeader}</h4>
         <p>2월 17일 발행</p>
+        <hr />
       </div>
     </div>
   );
