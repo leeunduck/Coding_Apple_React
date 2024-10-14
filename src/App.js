1e60e6edb5177405ec9baaff9bad0ceaa3f44e19
이언덕 <110515401+leeunduck@users.noreply.github.com>
2024-10-14 22:16:55 +0900

    Part1/lec3
    * feat: jsx start
    
    * feat: blog_nav
    
    * feat: JSX in variable
    
    * feat: data_binding

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  let color = "red";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>블로그</div>
+        <div className={color}>빨간색</div>
+      </div>
     </div>
   );
 }
