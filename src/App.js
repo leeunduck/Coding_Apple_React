29dd39474c6e5f45a1136389a5343d9789b0eae8
HyunseokLEE <unducklee@gmail.com>
2024-10-14 22:13:34 +0900

    feat: data_binding

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 
 function App() {
-  let post = "강남 우동 맛집";
+  let color = "red";
   return (
     <div className="App">
       <div className="black-nav">
         <div>블로그</div>
-        <div>{post}</div>
+        <div className={color}>빨간색</div>
       </div>
     </div>
   );
