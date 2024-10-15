526d1e6842c3b5b6a24ffb2aeeda29f4574300d2
이언덕 <110515401+leeunduck@users.noreply.github.com>
2024-10-15 21:43:26 +0900

    Part1/lec5
    * feat: onClick & useState
    
    * feat: homework

--- src/App.js
+++ src/App.js
@@ -5,13 +5,30 @@ function App() {
   let [cloth, setCloth] = useState("남자 코트 추천");
   let [list, setList] = useState("강남 우동 맛집");
   let [python, setPython] = useState("파이썬독학");
+  let [count, setCount] = useState(0);
+
   return (
     <div className="App">
       <div className="black-nav">
         <div>React blog</div>
       </div>
       <div className="list">
-        <h4>{cloth}</h4>
+        <button
+          onClick={() => {
+            setCloth("여자 코트 추천");
+          }}>
+          버튼
+        </button>
+        <h4>
+          {cloth}{" "}
+          <span
+            onClick={() => {
+              setCount(count + 1);
+            }}>
+            👍
+          </span>{" "}
+          {count}
+        </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
