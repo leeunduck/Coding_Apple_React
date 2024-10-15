3c6e9830cb4ecc9ffb7eccd4178f9826c664ec9f
HyunseokLEE <unducklee@gmail.com>
2024-10-15 21:39:49 +0900

    feat: onClick & useState

--- src/App.js
+++ src/App.js
@@ -5,13 +5,24 @@ function App() {
   let [cloth, setCloth] = useState("남자 코트 추천");
   let [list, setList] = useState("강남 우동 맛집");
   let [python, setPython] = useState("파이썬독학");
+  let [count, setCount] = useState(0);
+
   return (
     <div className="App">
       <div className="black-nav">
         <div>React blog</div>
       </div>
       <div className="list">
-        <h4>{cloth}</h4>
+        <h4>
+          {cloth}{" "}
+          <span
+            onClick={() => {
+              setCount(count + 1);
+            }}>
+            👍
+          </span>{" "}
+          {count}
+        </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
